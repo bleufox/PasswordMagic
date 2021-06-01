60bb77e9a0db5d428b1db18ee091463a3b74d505
bleufox <thebleufox8@gmail.com>
2021-05-31 17:07:46 -0700

    Final Upload of Password Magic

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# PasswordMagic
-Random password generator using Javascript
+# Password Magic Random Password Generator
+This application will generate a random password based criteria specified by the user. The user will click the red "Generate Password" button and they will be prompted to answer the following questions:
+
+1.) How many characters long would you like your password to be? (Between 8 - 128 characters.)
+2.) Would you like to include uppercase letters?
+3.) Would you like to include lowercase letters?
+4.) Would you like to include numbers?
+5.) Would you like to include special characters?
+
+# Screenshot of Application
+The following image shows the web applicagtions appearance and functionality:
+![Screenshot](images/Screenshot.png)
---
+++ js/script.js
@@ -0,0 +1,87 @@
+let password = "";
+
+let generatePassword = function () {
+
+  password = "";
+
+
+  let lengthPassword = passwordLength();
+
+
+  let inputLength = newPassword();
+  let userInput = inputLength.length;
+
+  for (let index = 0; index < lengthPassword; index++) {
+    password += inputLength.charAt(Math.floor(Math.random() * userInput));
+  }
+
+  return password;
+};
+
+
+let passwordLength = function () {
+
+  let passwordLengthPrompt = prompt("How many characters long would you like your password to be? (Between 8 - 128 characters.)");
+
+  if (passwordLengthPrompt < 8 || passwordLengthPrompt > 128 || passwordLengthPrompt === "") {
+    alert("You did not enter a number between 8 and 128. Please try again.")
+    return passwordLength;
+  }
+  
+  passwordLengthPrompt = Math.floor(Number(passwordLengthPrompt));
+
+  return passwordLengthPrompt;
+  
+}
+
+let newPassword = function () {
+
+let inputLength = "";
+
+  const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const numbers = '1234567890'
+  const specialChars = '`~!@#$%^&*()-_'
+
+  const uppercaseConfirm = confirm("Would you like to include uppercase letters?");
+  console.log(uppercaseConfirm);
+
+  const lowercaseConfirm = confirm("Would you like to include lowercase letters?");
+  console.log(lowercaseConfirm);
+
+  const numbersConfirm = confirm("Would you like to include numbers?");
+  console.log(numbersConfirm);
+
+  const specialCharsConfirm = confirm("Would you like to include special charactersrs?");
+  console.log(specialCharsConfirm);
+
+  if(uppercaseConfirm === true) {
+    inputLength += uppercaseLetters;
+  }
+
+  if(lowercaseConfirm === true) {
+    inputLength += lowercaseLetters;
+  }
+
+  if(numbersConfirm === true) {
+    inputLength += numbers;
+  }
+
+  if(specialCharsConfirm === true) {
+    inputLength += specialChars;
+  }
+
+    return inputLength;
+
+}
+
+const generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
